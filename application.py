9bb971327cb8c3f035cec253e168c25b66980ed9
Pedro Filho <pedro.filho@PPM-HOME-8290.local>
2020-07-31 01:15:58 -0300

    Packaging script

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! This is our pharos API!"
+    return "Pharos API"
 
 # run the app.
 if __name__ == "__main__":
---
+++ pack.sh
@@ -0,0 +1,1 @@
+zip package.zip server requirements.txt application.py
